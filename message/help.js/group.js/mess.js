bad0cadad49d808bfeeff2cc1d227a9a197c5c71
itsmegng <88885503+itsmegng@users.noreply.github.com>
2021-08-13 19:49:35 +0800

    Aku Agung Bau

---
+++ message/help.js/group.js/mess.js
@@ -0,0 +1,864 @@
+exports.gameMenu = (prefix) => {
+
+    return `
+
+ 🕹️ GAME-MENU 🕹️
+
+ 
+
+⚙️ ${prefix}slot
+
+⚙️ ${prefix}limitgame
+
+⚙️ ${prefix}gelud @tag
+
+⚙️ ${prefix}tictactoe @tag
+
+⚙️ ${prefix}siapaaku
+
+⚙️ ${prefix}family100
+
+⚙️ ${prefix}kuismath
+
+⚙️ ${prefix}asahotak
+
+⚙️ ${prefix}tebaklirik
+
+⚙️ ${prefix}tebaklagu
+
+⚙️ ${prefix}tebakkata
+
+⚙️ ${prefix}susunkata
+
+⚙️ ${prefix}kimiakuis
+
+⚙️ ${prefix}caklontong
+
+⚙️ ${prefix}tebakjenaka
+
+⚙️ ${prefix}tebakanime
+
+⚙️ ${prefix}tebaktebakan
+
+⚙️ ${prefix}tebakgambar
+
+⚙️ ${prefix}tebakbendera
+
+⚙️ ${prefix}suit *batu/kertas/gunting*
+
+`
+
+}
+
+exports.funMenu = (prefix) => {
+
+    return `
+
+   # FUN-MENU
+
+⚙️ ${prefix}mining
+
+⚙️ ${prefix}togel
+
+⚙️ ${prefix}cekwatak
+
+⚙️ ${prefix}cekmati [nama]
+
+⚙️ ${prefix}wangy [nama]
+
+⚙️ ${prefix}citacita
+
+⚙️ ${prefix}toxic
+
+⚙️ ${prefix}truth
+
+⚙️ ${prefix}dare
+
+⚙️ ${prefix}apakah
+
+⚙️ ${prefix}bisakah
+
+⚙️ ${prefix}kapankah
+
+⚙️ ${prefix}rate
+
+⚙️ ${prefix}jadian
+
+⚙️ ${prefix}cantik
+
+⚙️ ${prefix}ganteng
+
+⚙️ ${prefix}beban
+
+⚙️ ${prefix}babi
+
+⚙️ ${prefix}cekganteng
+
+⚙️ ${prefix}cekcantik
+
+`
+
+}
+
+exports.otherMenu = (prefix) => {
+
+    return `
+
+  #  OTHER-MENU 
+
+⚙️ ${prefix}brainly *query*
+
+⚙️ ${prefix}shopee *product*
+
+⚙️ ${prefix}playstore *query*
+
+⚙️ ${prefix}ssweb *query*
+
+⚙️ ${prefix}google *query*
+
+⚙️ ${prefix}image *query*
+
+⚙️ ${prefix}pinterest *query*
+
+⚙️ ${prefix}nulis *teks*
+
+⚙️ ${prefix}iguser *ussername*
+
+⚙️ ${prefix}igstalk *username*
+
+⚙️ ${prefix}githubstalk *username*
+
+⚙️ ${prefix}tiktokstalk *ussername*
+
+⚙️ ${prefix}img2url *reply foto*
+
+⚙️ ${prefix}ytsearch *query*
+
+`
+
+}
+
+exports.stickerMenu = (prefix) => {
+
+    return `
+
+  # STICKER-MENU
+
+⚙️ ${prefix}dadu
+
+⚙️ ${prefix}doge
+
+⚙️ ${prefix}toimg
+
+⚙️ ${prefix}patrick
+
+⚙️ ${prefix}garwgura
+
+⚙️ ${prefix}ttg *teks*
+
+⚙️ ${prefix}attp *teks*
+
+⚙️ ${prefix}stickeranime
+
+⚙️ ${prefix}semoji *emoji*
+
+⚙️ ${prefix}sticker *reply foto/video*
+
+⚙️ ${prefix}smeme *teks|teks*
+
+⚙️ ${prefix}swm *pack|author*
+
+⚙️ ${prefix}take *pack|author* 
+
+⚙️ ${prefix}tovideo *reply sgif*
+
+`
+
+}
+
+exports.wibuMenu = (prefix) => {
+
+    return `
+
+  # WIBU-MENU
+
+⚙️ ${prefix}loli
+
+⚙️ ${prefix}manga
+
+⚙️ ${prefix}anime 
+
+⚙️ ${prefix}lolivideo
+
+⚙️ ${prefix}husbu
+
+⚙️ ${prefix}waifu
+
+⚙️ ${prefix}milf
+
+⚙️ ${prefix}neko
+
+⚙️ ${prefix}kanna
+
+⚙️ ${prefix}sagiri
+
+⚙️ ${prefix}hentai
+
+⚙️ ${prefix}cosplay
+
+⚙️ ${prefix}wallnime
+
+⚙️ ${prefix}kusonime
+
+⚙️ ${prefix}megumin
+
+⚙️ ${prefix}otakudesu
+
+⚙️ ${prefix}doujindesu
+
+⚙️ ${prefix}storyanime
+
+⚙️ ${prefix}nakanomiku
+
+⚙️ ${prefix}nakanonino
+
+⚙️ ${prefix}nakanoitsuki
+
+⚙️ ${prefix}otakuongoing
+
+⚙️ ${prefix}nhentai *code*
+
+⚙️ ${prefix}nekopoi *link*
+
+⚙️ ${prefix}nekopoi3d
+
+⚙️ ${prefix}nekopoicosplay
+
+⚙️ ${prefix}nekopoisearch
+
+`
+
+}
+
+exports.ownerMenu = (prefix) => {
+
+    return `
+
+  # OWNER-MENU
+
+⚙️ ${prefix}bc *teks*
+
+⚙️ ${prefix}term
+
+⚙️ ${prefix}eval
+
+⚙️ ${prefix}reset
+
+⚙️ ${prefix}clearall
+
+⚙️ ${prefix}leaveall
+
+⚙️ ${prefix}join *link gc*
+
+⚙️ ${prefix}shutdown
+
+⚙️ ${prefix}getquoted
+
+⚙️ ${prefix}addupdate *fiturnya*
+
+⚙️ ${prefix}exif *nama|author*
+
+⚙️ ${prefix}sewa add/del *waktunya*
+
+⚙️ ${prefix}premium add @tag|nomor
+
+⚙️ ${prefix}premium del @tag|nomor
+
+`
+
+}
+
+exports.groupMenu = (prefix) => {
+
+    return`
+
+  # GROUP-MENU
+
+⚙️ ${prefix}groupsetting
+
+⚙️ ${prefix}afk *alasan*
+
+⚙️ ${prefix}ceksewa
+
+⚙️ ${prefix}kickall
+
+⚙️ ${prefix}infogrup
+
+⚙️ ${prefix}promote
+
+⚙️ ${prefix}demote
+
+⚙️ ${prefix}listonline
+
+⚙️ ${prefix}tagall *teks*
+
+⚙️ ${prefix}leave
+
+⚙️ ${prefix}kick *reply*
+
+⚙️ ${prefix}add *+62xxxxxx*
+
+⚙️ ${prefix}setgrupname
+
+⚙️ ${prefix}setppgrup
+
+⚙️ ${prefix}setdesc
+
+⚙️ ${prefix}sider *reply chat bot*
+
+⚙️ ${prefix}hidetag *teks/reply teks*
+
+,
+
+`
+
+}
+
+exports.downloadMenu = (prefix) => {
+
+    return `
+
+  ⬇️DOWNLOAD-MENU⬇️
+
+⚙️ ${prefix}fb 
+
+⚙️ ${prefix}igdl 
+
+⚙️ ${prefix}igdl2 
+
+⚙️ ${prefix}twitter 
+
+⚙️ ${prefix}tiktok 
+
+⚙️ ${prefix}play 
+
+⚙️ ${prefix}ythd 
+
+⚙️ ${prefix}ytmp3 
+
+⚙️ ${prefix}ytmp4 
+
+⚙️ ${prefix}soundcloud 
+
+⚙️ ${prefix}tiktoknowm 
+
+⚙️ ${prefix}tiktokaudio
+
+⚙️ ${prefix}mediafire 
+
+⚙️ ${prefix}nhentaipdf *code*
+
+`
+
+}
+
+exports.rulesBot = (prefix) => {
+
+    return `
+
+ # RULES BOT
+
+1. Jangan spam bot. 
+
+Sanksi: *WARN/SOFT BLOCK*
+
+2. Jangan telepon bot.
+
+Sanksi: *SOFT BLOCK*
+
+3. Jangan mengejek bot.
+
+Sanksi: *PERMANENT BLOCK*
+
+4. Gpp sc gw jelek yg penting 
+
+Bot gw jalan awokawoak
+
+Jika sudah dipahami rules-nya, silakan ketik *#menu* untuk memulai!
+
+Owner  BOTZ:
+
+wa.me/6285215319934
+
+`
+
+}
+
+exports.infoMenu = (prefix) => {
+
+return `
+
+  🌞  
+
+𝘚𝘢𝘪𝘥 𝘈𝘮𝘦𝘭 𝘉𝘰𝘵~ :
+
+Jangan menangis karena kegagalan cinta, sebab manusia akan meninggalkan semua yang dicintainya.
+
+╔═════◓「 𝘐𝘯𝘧𝘰𝘳𝘮𝘢𝘵𝘪𝘰𝘯 𝘉𝘰𝘵 」◓═══►
+
+║▹ 𝐆𝐫𝐨𝐮𝐩 𝐂𝐡𝐚𝐭𝐬 : *${groups.length}*
+
+║▹ 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 𝐂𝐡𝐚𝐭𝐬 : *${privat.length}*
+
+║▹ 𝐓𝐨𝐭𝐚𝐥 𝐂𝐡𝐚𝐭𝐬 : *${totalChat.length}*
+
+║▹ 𝐒𝐩𝐞𝐞𝐝 : *${latensie.toFixed(4)} _Second_*
+
+║▹ 
+
+║▹ 𝐏𝐥𝐚𝐭𝐟𝐨𝐫𝐦 : *${os.platform()}*
+
+║▹ *Batera*i : *${baterai}% ${charger}*
+
+╚═════════════════════▸►
+
+╔═════◓「 ɪɴғᴏ 𝘋𝘪𝘳𝘪𝘮𝘶 」◓═══►
+
+║▹ nama : ${pushname}
+
+║▹ member :
+
+╚═════════════════════▸►
+
+╔═════◓「 ᴀʙᴏᴜᴛ 」◓═══►
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+╚═════════════════════▸►
+
+╔═════◓「 ᴍᴜʟᴛɪ ᴇᴀᴏɴ 」◓═══►
+
+║▹ #
+
+║▹ #
+
+╚═════════════════════▸►
+
+╔═════◓「 ᴛɪᴄᴋᴇʀ ᴍᴇɴᴜ 」◓═══►
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+╚═════════════════════▸►
+
+╔═════◓「 ᴅᴏᴡɴʟᴏᴀᴅᴇʀ ᴍᴇɴᴜ 」◓═══►
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+╚═════════════════════▸►
+
+╔═════◓「 ᴅᴏᴡɴʟᴏᴀᴅᴇʀ ᴍᴇɴᴜ 」◓═══►
+
+║▹ # 
+
+╚═════════════════════▸►
+
+╔═════◓「 ᴛᴀʟᴋ ᴍᴇɴᴜ 」◓═══►
+
+║▹ # 
+
+║▹ # 
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ # 
+
+╚═════════════════════▸►
+
+╔═════◓「 ᴍᴇᴅɪᴀ ᴍᴇɴᴜ 」◓═══►
+
+║▹ #
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ #
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+╚═════════════════════▸►
+
+╔═════◓「 ᴛᴏᴏʟ ᴍᴇɴᴜ 」◓═══►
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # ?
+
+║▹ # 
+
+║▹ # 
+
+║▹ #
+
+╚═════════════════════▸►
+
+╔═════◓「 ʙᴀɪʟᴇʏ ᴍᴇɴᴜ 」◓═══►
+
+║▹ # #
+
+║▹ #
+
+║▹ #
+
+║▹ # 
+
+║▹ # 
+
+║▹ #
+
+║▹ # 
+
+║▹ #
+
+║▹ #   
+
+╚═════════════════════▸►
+
+╔═════◓「 𝘴𝘦𝘸𝘢 ᴍᴇɴᴜ 」◓═══►
+
+║▹ #
+
+║▹ #  
+
+║▹ # 
+
+╚═════════════════════▸►
+
+╔═════◓「 𝘱𝘳𝘦𝘮𝘪𝘶𝘮 ᴍᴇɴᴜ 」◓═══►
+
+║▹ #  
+
+║▹ # 
+
+║▹ #
+
+║▹ #
+
+╚═════════════════════▸►
+
+╔═════◓「 𝘣𝘢𝘯𝘯𝘦𝘥 ᴍᴇɴᴜ 」◓═══►
+
+║▹ # 
+
+║▹ # 
+
+║▹ #
+
+╚═════════════════════▸►
+
+╔═════◓「 𝘨𝘢𝘮𝘦 ᴍᴇɴᴜ 」◓═══►
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+╚═════════════════════▸►
+
+╔═════◓「 𝘵𝘦𝘹𝘵 𝘮𝘢𝘬𝘦𝘳 ᴍᴇɴᴜ 」◓═══►
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #                
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+╚═════════════════════▸►
+
+╔═════◓「 𝘨𝘳𝘶𝘱 ᴍᴇɴᴜ 」◓═══►
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+╚═════════════════════▸►
+
+╔═════◓「 𝘴𝘺𝘴𝘵𝘦𝘮 ᴍᴇɴᴜ 」◓═══►
+
+║▹ # 
+
+║▹ # 
+
+║▹ # 
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ # 
+
+╚═════════════════════▸►
+
+╔═════◓「 𝘰𝘸𝘯𝘦𝘳 ᴍᴇɴᴜ 」◓═══►
+
+║▹ >
+
+║▹ $
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+║▹ #
+
+╚═════════════════════▸►
+
+╔═════◓「 ᴄʀᴇᴅɪᴛ ᴛǫᴛᴏ 」◓═══►
+
+║▹ 𝘈𝘭𝘭𝘢𝘩 𝘚𝘸𝘵
+
+║▹ 𝘒𝘦𝘭𝘶𝘢𝘳𝘨𝘢
+
+║▹ 𝘏𝘢𝘧𝘪𝘥𝘻 𝘈𝘣𝘥𝘪𝘭𝘭𝘢𝘩
+
+║▹ _ _
+
+║▹ 𝘢𝘭𝘭 𝘤𝘳𝘦𝘢𝘵𝘰𝘳 𝘣𝘰𝘵~
+
+║▹ 𝘈𝘯𝘥 𝘒𝘢𝘭𝘪𝘢𝘯 𝘚𝘦𝘮𝘶𝘢 𝘗𝘦𝘯𝘨𝘨𝘶𝘯𝘢 𝘉𝘰𝘵~
+
+╚═════════════════════▸►
+
+`
+
+} 
+
